c104b258258e427c3b1bed8625b0097dea4e1b8c
kattababu@gmail.com <Mohanrao@>
2017-02-13 13:21:36 +0530

    Source

---
+++ src/Sample.java
@@ -0,0 +1,17 @@
+
+public class Sample {
+
+	public Sample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to India");
+
+	}
+
+}
