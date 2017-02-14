d20dbab9bbdae8c05b636126a22e3e6ff5511f89
kattababu@gmail.com <Mohanrao@>
2017-02-14 14:49:37 +0530

    Updated Files

---
+++ src/com/Nutch/Crawl/Canal/Happy.java
@@ -0,0 +1,18 @@
+package com.Nutch.Crawl.Canal;
+
+public class Happy {
+
+	public Happy() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Happy Birthday");
+
+	}
+
+}
