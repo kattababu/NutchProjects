705b0e7fb4123532918412b0aa8ec7004f7594d3
kattababu@gmail.com <Mohanrao@>
2017-02-23 11:09:43 +0530

    Updated Code

---
+++ src/JavaRunCommand.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class JavaRunCommand {
+
+    public static void main(String args[]) {
+
+        String cmdline="hrb@office.headrun.com";
+        String passwd="satishdhawan16!";
+        String cp=cmdline+passwd;
+        String cmd[]={"/bin/bash","-c",cp};
+        
+        
+       String filepath="/katta/CanalIN/FileToDb.sh";
+
+        try {
+        	 Process p = Runtime.getRuntime().exec(filepath);
+             p.waitFor();
+             InputStream in = p.getInputStream();
+             ByteArrayOutputStream baos = new ByteArrayOutputStream();
+              
+             int c = -1;
+             while((c = in.read()) != -1)
+             {
+                 baos.write(c);
+                // System.out.println(c);
+             }
+              
+             String response = new String(baos.toByteArray());
+             System.out.println("Response From Exe : "+response);
+            
+	           }
+        catch (Exception e) {
+            e.printStackTrace();
+                   }
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,37 @@
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+import java.io.File;
+
+public class test {
+public static void main(String args[]) throws JSchException {
+
+    JSch jsch = new JSch();
+    Session session = jsch.getSession("hrb", "office.headrun.com");//.getSession("hrb", "10.152.232.1", 22); //port is usually 22
+    session.setPassword("satishdhawan16!");
+    java.util.Properties config = new java.util.Properties(); 
+    config.put("StrictHostKeyChecking", "no");
+    session.setConfig(config);
+    //session.
+  // Session.put("StrictHostKeyChecking", "no");
+    session.connect();
+    Channel channel = session.openChannel("sftp");
+    channel.connect();
+    ChannelSftp cFTP = (ChannelSftp) channel;
+    JSch.setConfig("StrictHostKeyChecking", "no");
+    String sourceFile = "/katta/CanalIN/CanalIN_Terminal_movie_20170222T13225600073.queries", targetFile = "/Users/hrb/sathwick_nutch_files";
+    try {
+
+        cFTP.put(sourceFile , targetFile );
+    } catch (SftpException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+
+    cFTP.disconnect();
+    session.disconnect();
+}
+}
